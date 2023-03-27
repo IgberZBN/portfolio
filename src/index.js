22248073ccfc0807b6bec1e9388428f3d2197f3f
IgberZBN <igber_benitez@outlook.com>
2023-03-26 21:06:49 -0300

    Arrumando bug do background com js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
-import MobileMenu from './js/menu.js';
-import Carousel from './js/carousel.js';
+import MobileMenu from "./js/menu.js";
+import Carousel from "./js/carousel.js";
 
 const mobileMenu = new MobileMenu(
-  '[data-js-toggle]',
-  '.menu',
-  '.hero-nav-links',
+  "[data-js-toggle]",
+  ".menu",
+  ".hero-nav-links"
 );
 mobileMenu.init();
 
 const carousel = new Carousel(
-  '.carousel',
-  '.project',
+  ".carousel",
+  ".project",
   '[data-js-slide="previous"]',
   '[data-js-slide="next"]',
-  '[data-js-select]',
+  "[data-js-select]"
 );
 carousel.init();
+
+const documentHeight = () => {
+  const doc = document.documentElement;
+  doc.style.setProperty("--doc-height", `${window.innerHeight}px`);
+};
+window.addEventListener("resize", documentHeight);
+documentHeight();
